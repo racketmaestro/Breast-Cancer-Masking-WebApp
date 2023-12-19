4459cf8bdb4bb0fe4d00ce3285ed92b7b73f8c00
Amos <amos.koh.tk@gmail.com>
2023-12-19 19:27:42 +0000

    update conditional questions

--- src/UI/patient_interface.py
+++ src/UI/patient_interface.py
@@ -12,18 +12,42 @@ class PatientInputInterface:
         with open('options_config.json', 'r') as config_file:
             config = json.load(config_file)
 
+        # Initialize optional values
+        ageAtFirstChild = None
+        numBenignDiagnoses = None
+        atypicalHyperplasiaStatus = None
+        
         # add the options selection UI 
         age = st.slider("Age", **config['age_range'])
         ageMen = st.slider("Age of first Menstrual Period", **config['age_men_range'])
         ethnicity = st.selectbox("Ethnicity", config['ethnicities'])
+        relativesWithCancer = st.selectbox("Number of first degree relatives who had breast cacner", config['relatives_with_cancer'])
+        # Checkbox to ask if the user has a child
+        has_child = st.checkbox("Do you have a child?") 
+
+        # If the user has a child, show the slider to input age
+        if has_child:
+            ageAtFirstChild = st.slider("At what age did you have your first child?", 16, 50)
+
+        biopsy_status = st.radio("Have you ever had a biopsy for breast cancer?", ('Yes', 'No', 'Unknown'))
+
+        # Conditional questions based on the response to the main question
+        if biopsy_status == 'Yes':
+            # Question for the number of benign diagnoses
+            numBenignDiagnoses = st.radio("How many breast biopsies with benign diagnoses:", ('1', '2 or more'))
+
+            # Question for atypical hyperplasia
+            atypicalHyperplasiaStatus = st.radio("Have you ever had a breast biopsy with atypical hyperplasia?",
+                                            ('Yes', 'No', 'Unknown'))
+
         mammogram_image = st.file_uploader("Upload Mammogram Image", type=config['file_types'])
 
         # Submit button logic
         if st.button("Submit"):
             # if mammogram_image is None:
-            #     st.error("Please upload a mammogram")
+            #     st.error("Please   upload a mammogram")
             #     return
-            self.patient_data.set_data(age, ethnicity, mammogram_image)
+            self.patient_data.set_data(age, ageMen, ethnicity, relativesWithCancer, ageAtFirstChild, numBenignDiagnoses, atypicalHyperplasiaStatus, mammogram_image)
             self.handle_submit()
 
     def handle_submit(self):
--- src/patient_data.py
+++ src/patient_data.py
@@ -4,18 +4,33 @@ class PatientData:
 
     def __init__(self):
         self.age = None
+        self.ageMen = None
         self.ethnicity = None
+        self.relativesWithCancer = None
+        self.ageAtFirstChild = None
+        self.numBenignDiagnoses = None
+        self.atypicalHyperplasiaStatus = None
         self.mammogram_image = None
 
-    def set_data(self, age, ethnicity, mammogram_image):
+    def set_data(self, age, ageMen, ethnicity, relativesWithCancer, ageAtFirstChild, numBenignDiagnoses, atypicalHyperplasiaStatus, mammogram_image):
         self.age = age
+        self.ageMen = ageMen
         self.ethnicity = ethnicity
+        self.relativesWithCancer = relativesWithCancer
+        self.ageAtFirstChild = ageAtFirstChild
+        self.numBenignDiagnoses = numBenignDiagnoses
+        self.atypicalHyperplasiaStatus = atypicalHyperplasiaStatus
         self.mammogram_image = mammogram_image
 
     # Test function
     def get_data_summary(self):
         return {
             "Age": self.age,
+            "Agemen" : self.ageMen,
             "Ethnicity": self.ethnicity,
+            "Relatives with cancer": self.relativesWithCancer,
+            "Age at first child": self.ageAtFirstChild,
+            "Number of benign diagnoses" : self.numBenignDiagnoses,
+            "Atypical Hyperplasia Status": self.atypicalHyperplasiaStatus,
             "Mammogram Image": "Uploaded" if self.mammogram_image is not None else "Not Uploaded"
         }
