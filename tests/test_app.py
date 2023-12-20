a5c5cf908c11081e9ec7dd577e78a35970ac33bf
Amos <amos.koh.tk@gmail.com>
2023-12-20 23:29:25 +0000

    create cancer prediction feature

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,5 @@ from streamlit.testing.v1 import AppTest
 def test_app():
     '''This uses the streamlit AppTest api to simulate a running app'''
     at = AppTest.from_file("app.py")
-    at.run()
+    at.run(timeout = 30)
     assert not at.exception 
