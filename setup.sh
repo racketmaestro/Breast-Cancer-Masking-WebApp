b39c4ce8bc418a76d18da8e5b87a6153db39dcb6
Amos <amos.koh.tk@gmail.com>
2023-12-20 15:43:45 +0000

    change theme

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
-[theme]
-base = 'light'
-primaryColor = '#96ead7'
-secondaryBackgroundColor = '#b8dbd3'
-font = 'sans serif'
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
+[theme]\n\
+base = 'light'\n\
+primaryColor = '#96ead7'\n\
+secondaryBackgroundColor = '#b8dbd3'\n\
+font = 'sans serif'\n\
 " > ~/.streamlit/config.toml
