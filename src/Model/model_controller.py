0b5616c38af1b753a9917067af18dc14ffde09a1
Amos <amos.koh.tk@gmail.com>
2024-01-14 01:05:41 +0000

    change requirements

--- src/Model/model_controller.py
+++ src/Model/model_controller.py
@@ -140,7 +140,7 @@ class ModelController:
             # Check that the probability output is above a certain threshold
             probability_of_chosen_class = prediction[0, birads_classification - 1]
             print(prediction)
-            if probability_of_chosen_class <= 0.8:
+            if probability_of_chosen_class <= 0.5:
                 st.error('''
                         Our model could not decisively predict your breast density based on the mammogram, 
                         perhaps check that you have uploaded the correct image or find another mammogram''')
