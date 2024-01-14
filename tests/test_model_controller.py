eafa182e4204b25e609342db8b20d7636da7db8a
Amos <amos.koh.tk@gmail.com>
2024-01-14 01:08:02 +0000

    remove test

--- tests/test_model_controller.py
+++ tests/test_model_controller.py
@@ -11,22 +11,3 @@ def test_init(model_controller):
     """
     assert model_controller.birad_classification_model is not None, "Model should be loaded"
 
-def test_predict_cancer(model_controller):
-    """
-    Test the predict_cancer function with a mock image file.
-    """
-    # Create a mock image file
-    from PIL import Image
-    import io
-
-    # Create a simple black square image
-    img = Image.new('L', (128, 128), color = 'black')
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    img_byte_arr = io.BytesIO(img_byte_arr.getvalue())
-
-    # Test the predict_cancer function
-    prediction = model_controller.predict_birad_classification(img_byte_arr)
-    assert prediction is not None, "predict_cancer should return a prediction"
-
-
