03b8aba06c1cbb96d30e23db5dd7e1aea3611b32
magichampz <91732309+magichampz@users.noreply.github.com>
2024-01-14 00:54:35 +0000

    added model code and preprocessing

--- src/Model/model_controller.py
+++ src/Model/model_controller.py
@@ -5,11 +5,12 @@ import streamlit as st
 from keras.models import load_model
 from src.patient_data import PatientData
 from src.Model.model_data import ModelData
+from src.Model.preprocessing_density import find_roi
 from PIL import Image
 from src.Model.Gail_ModelV5 import RiskModel
 
-class ModelController:
 
+class ModelController:
     # Define integer representations for race
     RACE_MAPPING = {
         "White": 1,
@@ -22,21 +23,16 @@ class ModelController:
         "Filipino": 8,
         "Hawaiian": 9,
         "Other Pacific Islander": 10,
-        "Other Asian": 11
+        "Other Asian": 11,
     }
 
     # Define integer representations for relatives
-    RELATIVES_MAPPING = {
-        "None": 0,
-        "One": 1,
-        "More than one": 2
-    }
+    RELATIVES_MAPPING = {"None": 0, "One": 1, "More than one": 2}
 
     def __init__(self) -> None:
-
         try:
             # Load the birad classification model
-            birad_model_path = 'src/Model/BiradClassificationModel.h5'
+            birad_model_path = "src/Model/BiradClassificationModel.h5"
             self.birad_classification_model = load_model(birad_model_path)
         except Exception as e:
             # Handle exceptions
@@ -44,8 +40,8 @@ class ModelController:
         pass
 
     def generate_input_data(self, patient_data: PatientData) -> ModelData:
-        '''This function will transform the questionnaire answers into formats
-        compatible with the model, and return the ModelData data class'''
+        """This function will transform the questionnaire answers into formats
+        compatible with the model, and return the ModelData data class"""
 
         # Create new instance of ModelData class
         model_data = ModelData()
@@ -53,12 +49,22 @@ class ModelController:
         # Populate the fields
         model_data.age = patient_data.age
         model_data.age_menstruation = patient_data.age_men
-        model_data.age_first_child = patient_data.age_at_first_child if patient_data.age_at_first_child is not None else 98
-        model_data.birad_classification = patient_data.birad_classification if patient_data.birad_classification is not None else 1
+        model_data.age_first_child = (
+            patient_data.age_at_first_child
+            if patient_data.age_at_first_child is not None
+            else 98
+        )
+        model_data.birad_classification = (
+            patient_data.birad_classification
+            if patient_data.birad_classification is not None
+            else 1
+        )
         # model_data.BiRads = patient_data.birad_classification
 
         # Map number of relatives answer to an integer
-        model_data.num_relatives = ModelController.RELATIVES_MAPPING.get(patient_data.relatives_with_cancer, 99)
+        model_data.num_relatives = ModelController.RELATIVES_MAPPING.get(
+            patient_data.relatives_with_cancer, 99
+        )
 
         # Default values for both variables
         model_data.num_biopsies = 99
@@ -88,8 +94,8 @@ class ModelController:
         return model_data
 
     def predict_risk(self, model_data: ModelData):
-        '''This function will run the risk analysis using the Gail Model and the processed health data input by users'''
-        
+        """This function will run the risk analysis using the Gail Model and the processed health data input by users"""
+
         model_data_json = model_data.to_dict()
         data = pd.DataFrame([model_data_json])
 
@@ -97,35 +103,43 @@ class ModelController:
             risk_model = RiskModel(data)
             risk_output = risk_model.run_model()
         except Exception as e:
-            st.error(f"An error occured while running risk evaluation, please report it to Silcock and Sons: {e}")
-        
+            st.error(
+                f"An error occured while running risk evaluation, please report it to Silcock and Sons: {e}"
+            )
+
         return risk_output
-    
+
     def predict_birad_classification(self, uploaded_file):
-        '''This function will use the mammogram uploaded to predict the BiRads classification of the user'''
+        """This function will use the mammogram uploaded to predict the BiRads classification of the user"""
         # Read the file into a bytes-like object
         image_data = uploaded_file.read()
 
         # Open the image with PIL (ensures compatibility with different file types)
         image = Image.open(io.BytesIO(image_data))
 
-        # Convert the image to grayscale
-        if image.mode != 'L':
-            image = image.convert('L')
+        # Don't grayscale
+        # # Convert the image to grayscale
+        # if image.mode != 'L':
+        #     image = image.convert('L')
 
         # Transform the image to fit the model input requirements
-        image = image.resize((128, 128))
         image_array = np.array(image)
-        image_array = np.expand_dims(image_array, axis=0)
+        image_array = np.expand_dims(image_array, axis=2)
+        image_array = np.repeat(image_array, 3, 2)
+
+        cropped_image = find_roi(image_array)
+
+        cropped_image = np.expand_dims(cropped_image, axis=0)
 
         # Predict probability of each BiRads classification
         try:
-            prediction = self.birad_classification_model(image_array)
+            prediction = self.birad_classification_model(cropped_image)
         except Exception as e:
-            st.error(f"An error occurred while trying to predict BiRads classification: {e}")
+            st.error(
+                f"An error occurred while trying to predict BiRads classification: {e}"
+            )
 
         # Set the classification to the category with highest probability. + 1 since zero-index
-        birads_classification = np.argmax(prediction) + 1 
+        birads_classification = np.argmax(prediction) + 1
 
         return birads_classification
-
---
+++ src/Model/preprocessing_density.py
@@ -0,0 +1,127 @@
+"""
+Function to process and crop out the region of interest for the mammogram images
+"""
+
+import numpy as np
+import cv2
+import copy
+
+
+def crop_roi(mask, og_image, height_og, width_og):
+    crop1 = mask[
+        int(height_og * 0.05) : int(height_og * 0.95),
+        int(width_og * 0.03) : int(width_og * 0.97),
+    ]
+    imcopy = copy.copy(crop1)
+
+    crop1 = imcopy[:]
+
+    height, width = crop1.shape
+
+    smaller_crop = crop1[int(height / 3) : int(height * 2 / 3)]
+    points = np.argwhere(smaller_crop == 255)  # find where the black pixels are
+    xpoints = points[:, 1]  # the x values
+
+    # check left or right, get sum of pixels of each half of image
+    area_L = crop1[:, : int(width / 2)]
+    area_R = crop1[:, int(width / 2) :]
+
+    # if crop1[int(height/2),0] == 255: # breast on left side
+    if np.sum(area_L) > np.sum(area_R):  #  left side
+        # print('left')
+        maxpoint = np.max(xpoints)
+        # print(maxpoint)
+        b = points[:, 1] == maxpoint
+        loc = np.nonzero(b > 0)[0][0]
+        small_y, x = points[loc]
+
+        y = small_y + height / 3
+
+        # print(loc)
+        # print(y,x)
+
+        box_wid = int(width * 0.6)
+        box_hei = int(height * 0.6)
+
+        y = int(y + 0.05 * height_og)
+        x = int(x + 0.03 * width_og)
+
+        x_start = x - box_wid
+
+        if x_start < 1:
+            x_start = 0
+        # saving the cropped roi image
+        final_img = og_image[int(y - box_hei / 2) : int(y + box_hei / 2), x_start:x]
+
+    elif np.sum(area_R) > np.sum(area_L):  # breast on right side
+        # print('right')
+        maxpoint = np.min(xpoints)
+        # print(maxpoint)
+
+        b = points[:, 1] == maxpoint
+        loc = np.nonzero(b > 0)[0][0]
+        small_y, x = points[loc]
+
+        y = small_y + height / 3
+
+        # print(loc)
+        # print(y,x)
+
+        box_wid = int(width * 0.6)
+        box_hei = int(height * 0.6)
+
+        y = int(y + 0.05 * height_og)
+        x = int(x + 0.03 * width_og)
+
+        x_end = x + box_wid
+
+        if x_end > (width_og - 1):
+            x_end = width_og - 1
+
+        final_img = og_image[int(y - box_hei / 2) : int(y + box_hei / 2), x:x_end]
+        # cv2.rectangle(img_copy, ( x, int(y-box_hei/2)), (x+box_wid, int(y+box_hei/2)), (255, 0, 0) , 20)
+
+    else:
+        print("error with image")
+
+    return final_img
+
+
+def find_roi(img):
+    img_copy = copy.copy(img[:, :, :])
+
+    height_og, width_og, c = img.shape
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
+    mask = np.zeros(
+        gray_img.shape, dtype=np.uint8
+    )  # mask initialized to image dimensions
+    blur_img = cv2.GaussianBlur(gray_img, (41, 41), 0)  # blur used to normalise image
+    ret, binary = cv2.threshold(blur_img, 25, 255, cv2.THRESH_BINARY)  # threshold image
+
+    contours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    if len(contours) <= 1:
+        # basically this means that the breast takes up the whole of the mammogram,
+        # so might as well just take the entire crop 1 region
+
+        # final_img  = img_copy[int(height_og*0.05):int(height_og*0.95)]
+        mask = binary
+        final_img = crop_roi(mask, img_copy, height_og, width_og)
+
+    else:
+        for i in contours:
+            x, y, w, h = cv2.boundingRect(i)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 20)
+            area = w * h
+
+            # only the biggest rectangle considered, ignore small ones
+            if (
+                area > 0.3 * height_og * width_og
+            ):  # 50% of area chosen for now, somewhat arbitrarily
+                cv2.drawContours(mask, [i], -1, (255, 255, 255), -1)
+
+        final_img = crop_roi(mask, img_copy, height_og, width_og)
+        final_img = cv2.resize(final_img, (128, 128))
+
+    return final_img
